c0164a3212d8cc5e0102019dd492848b907124d4
edeoj <edeoj@DESKTOP-QHMOVK4>
2022-12-09 17:30:21 +0100

    This is the Home pages

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,33 @@
-﻿using GasPOS.Models;
+﻿using GasPOS.Db;
+using GasPOS.IHelpers;
+using GasPOS.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 
 namespace GasPOS.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly AppDbContext _context;
+        private readonly ILogger<HomeController> _logger;
+        private readonly IAdminHelpers _adminHelpers;
+        public HomeController(AppDbContext context, ILogger<HomeController> logger, IAdminHelpers adminHelpers)
         {
             _logger = logger;
+            _context = context;
+            _adminHelpers = adminHelpers;
+            
         }
+
+
 
         public IActionResult Index()
         {
-            return View();
+            var topCynlinder = _adminHelpers.ListOfCynlinder().OrderByDescending(x => x.DateCreated).Take(6).ToList();
+            return View(topCynlinder);
         }
 
         public IActionResult Privacy()
